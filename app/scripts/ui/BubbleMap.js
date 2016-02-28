e0b738561b9ce3a78d5083d5dd2007c7b85dd611
David Jay <davidgljay@gmail.com>
2016-02-28 14:46:17 -0500

    Removing unused wrap function.

--- app/scripts/ui/BubbleMap.js
+++ app/scripts/ui/BubbleMap.js
@@ -83,8 +83,7 @@ var BubbleMap = React.createClass({
         .attr('id', function(d){return idify(d.tag)})
         .text(function(d){return prettify(d.tag)})
         .attr("dy",".32em")
-        .attr('text-anchor', 'middle')
-        .each(wrap);
+        .attr('text-anchor', 'middle');
 
 
       var force = d3.layout.force()
@@ -204,30 +203,6 @@ function prettify(tag) {
 
 function idify(tag) {
   return tag.replace(/[^a-z0-9_]/ig,"");
-}
-
-function wrap(width) {
-  return function() {
-    var text = d3.select(this),
-        words = text.text().split(/\s+/).reverse(),
-        word,
-        line = [],
-        lineNumber = 0,
-        lineHeight = 1.1, // ems
-        y = text.attr("y"),
-        dy = parseFloat(text.attr("dy")),
-        tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
-    while (word = words.pop()) {
-      line.push(word);
-      tspan.text(line.join(" "));
-      if (tspan.node().getComputedTextLength() > width) {
-        line.pop();
-        tspan.text(line.join(" "));
-        line = [word];
-        tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
-      }
-    }
-  }
 }
 
 function getRadiusScale(json) {
