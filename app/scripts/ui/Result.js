55fcf913e94606fbdbe2cbf4d4a91b29da38a787
David Jay <davidgljay@gmail.com>
2016-04-03 17:56:45 -0400

    Adding formatting to results.

--- app/scripts/ui/Result.js
+++ app/scripts/ui/Result.js
@@ -1,10 +1,23 @@
+var dateFormat = require('dateformat');
+
 var Result = (props) => (
-	<li className="list-group-item">{props.title}</li>
+	<div className="row result">
+		<div className="col-md-2 header">
+			<p><b>{dateFormat(props.date,"mmmm dS, yyyy")}</b></p>
+			<p>{props.city}</p>
+		</div>
+		<div className="col-md-8 body">
+			<h3>{props.title}</h3>
+			<span className="label label-pill label-default person">Person 1</span>
+		</div>
+	</div>
   );
 
 Result.propTypes = { title: React.PropTypes.string};
 Result.defaultProps = { 
-   title:'Result'
+   title:'Result',
+   date:new Date(),
+   city:'New York City'
 };
 
 module.exports=Result;
--- app/scripts/ui/Results.js
+++ app/scripts/ui/Results.js
@@ -1,20 +1,32 @@
 var Result = require('./Result');
 
 var Results = (props) => (
-  <div id="Results">
-  	<ul className="list-group">
-  		{props.data.map(function(item) {
-  			return <Result key={item.title} title={item.title}/>
-  		})}
-  	</ul>
+  <div>
+    <div id="result_metadata">
+      Stuff and things
+    </div>
+    <div id="results">
+    		{props.data.map(function(item) {
+    			return <Result key={item.title} title={item.title}/>
+    		})}
+    </div>
   </div>
   );
 
 Results.propTypes = { data: React.PropTypes.array};
 Results.defaultProps = { 
+
    data:[
-   	{title:'Result'},
-   	{title:'Another result'}
+   	{
+      title:'Result',
+      date: new Date(),
+      city: 'New York'
+  },
+   	{
+      title:'Another result',
+      date: new Date(),
+      city: 'Los Angeles'
+    }
    ]
 };
 
